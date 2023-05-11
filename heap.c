cd240fdbabea64ac780166a468e342525c46ab98
Replit user <>
2023-05-11 17:05:52 +0000

    new try

--- heap.c
+++ heap.c
@@ -27,11 +27,41 @@ void* heap_top(Heap* pq)
 
 
 
-void heap_push(Heap* pq, void* data, int priority){
+void heap_push(Heap* pq, void* data, int priority)
+{
+  if (pq->size >= pq->capac)
+  {
+    pq->heapArray = realloc(pq->heapArray, pq->capac * 2); 
+    
+  }
+  pq->heapArray[pq->size - 1].data = data;
+  pq->heapArray[pq->size - 1].priority = priority;
+  int posicionActual = pq->size - 1;
+  pq->size++;
+  int padrePosicion = (posicionActual - 1) / 2;
+  
+  while (pq->heapArray[posicionActual].priority > 
+          pq->heapArray[padrePosicion].priority)
+  {
+    if (pq->heapArray[padrePosicion].priority < 
+        pq->heapArray[posicionActual].priority)      
+    {
+      void *aux = pq->heapArray[posicionActual].data;
+      int prAux = pq->heapArray[posicionActual].priority; 
+      pq->heapArray[posicionActual].data = pq->heapArray[padrePosicion].data;
+      pq->heapArray[posicionActual].priority = 
+        pq->heapArray[padrePosicion].priority;
+      
+      pq->heapArray[padrePosicion].data = aux;
+      pq->heapArray[padrePosicion].priority = prAux;
+      
+      return;
+    }
+    posicionActual--;
+    padrePosicion = (posicionActual - 1) / 2;
+  }
 
 }
-
-
 void heap_pop(Heap* pq){
 
 }
